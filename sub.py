d74ac6f142b70689e49365c2531c3c56e28aa3c1
hckwon767 <97076595+hckwon767@users.noreply.github.com>
2025-03-19 14:48:13 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 url = "https://subapi.imgki.com/sub?target=clash&url=https%3A%2F%2Fraw.githubusercontent.com%2Fmai19950%2Fclash_config%2Frefs%2Fheads%2Fmain%2Fsub%2Fclash.yaml&insert=false&emoji=true&list=true&tfo=false&scv=true&fdn=false&expand=true&sort=false&new_name=true"
-filter_strings = ["abshare", "toshare", "ripaojiedian", "mkshare", "tolinkshare", "mksshare", " ss"]
+filter_strings = ["abshare", "toshare", "ripaojiedian", "mkshare", "tolinkshare", "mksshare"]
 
 try:
     response = requests.get(url)
