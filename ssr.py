897012f412c4b4ffcac5f6e32767b5366b8ca8c9
hckwon767 <97076595+hckwon767@users.noreply.github.com>
2025-04-11 10:56:29 +0800

    Update ssr.py

--- ssr.py
+++ ssr.py
@@ -137,7 +137,8 @@ if __name__ == "__main__":
                 nodeinfo.encode()
             ).decode().rstrip("=")
             SSR += ssr + "\n"
-        with open("./ssr", "w") as f:
+        os.makedirs("./links", exist_ok=True) # 디렉토리 생성 (이미 존재해도 에러 안남)
+        with open("./links/ssr", "w") as f:
             f.write(base64.b64encode(SSR.encode()).decode())
     except Exception as E:
         print(E)
